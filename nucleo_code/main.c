16fff11c29e3cc264052ae2a8a135815fbba3ac9
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-04-16 21:20:25 +0200

    added reading MPU-9502

--- nucleo_code/main.c
+++ nucleo_code/main.c
@@ -8,6 +8,9 @@
 #include "mutex.h"
 #include "servo.h"
 #include "thread.h"
+#include <inttypes.h>
+#include "mpu9x50.h"
+#include "mpu9x50_params.h"
 
 //define I2C parameters
 #define I2C_INTERFACE I2C_DEV(0)
@@ -89,6 +92,48 @@ static void* threadControl(void* arg)
 			return NULL;
 		}
 		
+	// definition of MPU-device and results 
+	mpu9x50_t dev;
+    mpu9x50_results_t measurement;
+    int32_t temperature;
+    int result;
+
+    // MPU device initialization
+    printf("+------------Initializing------------+\n");
+    result = mpu9x50_init(&dev, &mpu9x50_params[0]);
+    
+    // Check errors
+    if (result == -1) {
+        puts("[Error] The given i2c is not enabled");
+        return NULL;
+    }
+    else if (result == -2) {
+        puts("[Error] The compass did not answer correctly on the given address");
+        return NULL;
+    }
+    
+    // Definition of sample rates
+    mpu9x50_set_sample_rate(&dev, 200);
+    if (dev.conf.sample_rate != 200) {
+        puts("[Error] The sample rate was not set correctly");
+        return NULL;
+    }
+    mpu9x50_set_compass_sample_rate(&dev, 100);
+    if (dev.conf.compass_sample_rate != 100) {
+        puts("[Error] The compass sample rate was not set correctly");
+        return NULL;
+    }
+    
+    printf("Initialization successful\n\n");
+    printf("+------------Configuration------------+\n");
+    printf("Sample rate: %"PRIu16" Hz\n", dev.conf.sample_rate);
+    printf("Compass sample rate: %"PRIu8" Hz\n", dev.conf.compass_sample_rate);
+    printf("Gyro full-scale range: 2000 DPS\n");
+    printf("Accel full-scale range: 2 G\n");
+    printf("Compass X axis factory adjustment: %"PRIu8"\n", dev.conf.compass_x_adj);
+    printf("Compass Y axis factory adjustment: %"PRIu8"\n", dev.conf.compass_y_adj);
+    printf("Compass Z axis factory adjustment: %"PRIu8"\n", dev.conf.compass_z_adj);
+	
 	
 	void* args=arg;
 	args=NULL;
@@ -96,17 +141,36 @@ static void* threadControl(void* arg)
 	if(args==NULL)
 		printf(" ");
 	
-	char buffer[CONTROL_BUFFER_SIZE]={0};
-	int degree=0;
+	//char buffer[CONTROL_BUFFER_SIZE]={0};
+	//int degree=0;
 	
 	
 	while(1)
 	{
-		I2CCommunication(CONTROL_CMD,buffer,CONTROL_BUFFER_SIZE,"CONTROL");
+		//I2CCommunication(CONTROL_CMD,buffer,CONTROL_BUFFER_SIZE,"CONTROL");
 		//transformstring into integer in orderto control the servo in the correct way
-		degree=atoi(buffer);
-		controlServoOrientation(&servo,degree);
-		printf("[CONTROL]MEX: %i \n",degree);
+		//degree=atoi(buffer);
+		//controlServoOrientation(&servo,degree);
+		//printf("[CONTROL]MEX: %i \n",degree);
+		
+		/* Get accel data in milli g */
+        mpu9x50_read_accel(&dev, &measurement);
+        printf("Accel data [milli g] - X: %"PRId16"   Y: %"PRId16"   Z: %"PRId16"\n",
+                measurement.x_axis, measurement.y_axis, measurement.z_axis);
+        /* Get gyro data in dps */
+        mpu9x50_read_gyro(&dev, &measurement);
+        printf("Gyro data [dps] - X: %"PRId16"   Y: %"PRId16"   Z: %"PRId16"\n",
+                measurement.x_axis, measurement.y_axis, measurement.z_axis);
+        /* Get compass data in mikro Tesla */
+        mpu9x50_read_compass(&dev, &measurement);
+        printf("Compass data [micro T] - X: %"PRId16"   Y: %"PRId16"   Z: %"PRId16"\n",
+                measurement.x_axis, measurement.y_axis, measurement.z_axis);
+        /* Get temperature in milli degrees celsius */
+        mpu9x50_read_temperature(&dev, &temperature);
+        printf("Temperature [milli deg] : %"PRId32"\n", temperature);
+        printf("\n+-------------------------------------+\n");
+		
+		
 		xtimer_sleep(DELAY_CONTROL);
 	}
 	
