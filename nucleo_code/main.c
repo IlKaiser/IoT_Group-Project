aac5ddb3f598a77afcbedb768d94c4b60a671b95
IvanGiacomoni <62717293+IvanGiacomoni@users.noreply.github.com>
2021-04-19 18:17:27 +0200

    added power management

--- nucleo_code/main.c
+++ nucleo_code/main.c
@@ -11,6 +11,7 @@
 #include <inttypes.h>
 #include "mpu9x50.h"
 #include "mpu9x50_params.h"
+#include "fmt.h"
 
 //define I2C parameters
 #define I2C_INTERFACE I2C_DEV(0)
@@ -28,11 +29,20 @@
 #define ALARM_CMD 1
 #define ALARM_THRESHOLD 2
 
-//time interval for each thread
-#define DELAY_INFO 30
-#define DELAY_ALARM 1
-#define DELAY_CONTROL 1000000
+//base time interval for each thread
+#define DELAY_INFO_BASE 30
+#define DELAY_ALARM_BASE 1
+#define DELAY_CONTROL_BASE 1000000
 
+//medium time interval for each thread
+#define DELAY_INFO_MEDIUM 50
+#define DELAY_ALARM_MEDIUM 10
+#define DELAY_CONTROL_MEDIUM 10000000
+
+//high time interval for each thread
+#define DELAY_INFO_HIGH 70
+#define DELAY_ALARM_HIGH 20
+#define DELAY_CONTROL_HIGH 6000000
 
 //constant for servomotor
 #define SERVO_MIN 550
@@ -44,6 +54,12 @@
 #define FROM_RIGHT_TO_LEFT_CORRECTION_THRESHOLD 1
 #define FROM_LEFT_TO_RIGHT_CORRECTION_THRESHOLD -1
 
+//acceleration constants for power management ( m/s^2 )
+#define LOW_PM_THRESHOLD 9.5
+#define HIGH_PM_THRESHOLD 10.0
+
+//gravity acceleration constant
+#define GRAVITY_ACCELERATION 9.8066
 
 static char stackThreadControl[THREAD_STACKSIZE_DEFAULT];
 static char stackThreadAlarm[THREAD_STACKSIZE_DEFAULT];
@@ -51,6 +67,9 @@ static char stackThreadInfo[THREAD_STACKSIZE_DEFAULT];
 
 //define mutex to protect i2c access
 mutex_t mutex;
+
+//define delay variables for each thread
+int delay_info = DELAY_INFO_BASE, delay_control = DELAY_CONTROL_BASE, delay_alarm = DELAY_ALARM_BASE;
 
 
 void I2CCommunication(int cmd,void*buffer,int len,char* service)
@@ -89,8 +108,17 @@ void controlDCMotor(gpio_t dc_motor,int status)
 	
 }
 
+char* fromFloatToString(float f){
+    
+    char* buf = (char*)malloc(10*sizeof(char));
+    unsigned precision = 2;
+	size_t n = fmt_float(buf, f, precision);
+	buf[n] = '\0';
+    
+    return buf;
+}
 
-void getControlData(mpu9x50_t dev,char* correctionData,int len, short* x_axis)
+void getControlData(mpu9x50_t dev,char* correctionData,int len, short* x_compass, short* z_accel)
 {
 		//Data structure to save info
 		mpu9x50_results_t measurement;
@@ -98,7 +126,8 @@ void getControlData(mpu9x50_t dev,char* correctionData,int len, short* x_axis)
 	    mutex_lock(&mutex);
 	
 		/* Get accel data in milli g */
-        //mpu9x50_read_accel(&dev, &measurement);
+        mpu9x50_read_accel(&dev, &measurement);
+        *z_accel = measurement.z_axis;
         //printf("[MPU9250] Accel data [milli g] - X: %"PRId16"   Y: %"PRId16"   Z: %"PRId16"\n",
         //        measurement.x_axis, measurement.y_axis, measurement.z_axis);
         /* Get gyro data in dps */
@@ -110,7 +139,7 @@ void getControlData(mpu9x50_t dev,char* correctionData,int len, short* x_axis)
         //printf("[MPU9250] Compass data [micro T] - X: %"PRId16"   Y: %"PRId16"   Z: %"PRId16"\n",
         //        measurement.x_axis, measurement.y_axis, measurement.z_axis);
         
-        *x_axis = measurement.x_axis;
+        *x_compass = measurement.x_axis;
         
         
         printf("\n+-------------------------------------+\n");
@@ -124,6 +153,31 @@ void getControlData(mpu9x50_t dev,char* correctionData,int len, short* x_axis)
 	
 }
 
+void powerManagement(float z_accel){
+    if(z_accel <= LOW_PM_THRESHOLD){
+		printf("ACCEL LOW!\n");
+		    
+		delay_alarm = DELAY_ALARM_BASE;
+		delay_control = DELAY_CONTROL_BASE;
+		delay_info = DELAY_INFO_BASE;	
+	}
+    
+    else if(z_accel > LOW_PM_THRESHOLD && z_accel <= HIGH_PM_THRESHOLD){
+		printf("ACCEL MEDIUM!\n");
+		    
+		delay_alarm = DELAY_ALARM_MEDIUM;
+		delay_control = DELAY_CONTROL_MEDIUM;
+		delay_info = DELAY_INFO_MEDIUM;	
+	}
+		
+	else if(z_accel > HIGH_PM_THRESHOLD){
+		printf("ACCEL HIGH!\n");
+		    
+		delay_alarm = DELAY_ALARM_HIGH;
+		delay_control = DELAY_CONTROL_HIGH;
+		delay_info = DELAY_INFO_HIGH;	
+	}
+}
 
 static void* threadControl(void* arg)
 {
@@ -185,48 +239,59 @@ static void* threadControl(void* arg)
         
     printf("[MPU9250] Initialization successful\n\n");
 
-	int  i=0;
-	short x_start = 0;
+	int i=0;
+	short x_start_compass = 0;
 	
 	//get control data correction degree will be saved into buffer with sintax -360/360 as char*
 	while(1)
 	{
 		
-		//sample values for magnetic field detected by the compass
-        short x_axis;
+		//sample values for magnetic field detected by the compass and accelerometer
+        short x_compass, z_accel;
 		
 		//get control data correction degree will be saved into buffer with sintax -360/360 as char*
-	    getControlData(dev,buffer,CONTROL_BUFFER_SIZE, &x_axis);	
+	    getControlData(dev,buffer,CONTROL_BUFFER_SIZE, &x_compass, &z_accel);	
 		
 		if(i==1){
-		    x_start = x_axis;	
+		    x_start_compass = x_compass;	
 		}
 		
-		printf("x_start=%hd\n",x_start);
+		printf("x_start_compass=%hd\n",x_start_compass);
 		
-		printf("[MPU9250] Compass data [micro T] - X: %hd\n", x_axis);
+		printf("[MPU9250] Compass data [micro T] - X: %hd\n", x_compass);
 		
-		short x_diff = x_start - x_axis;
+		short x_diff_compass = x_start_compass - x_compass;
 		
-		if(x_diff > FROM_RIGHT_TO_LEFT_CORRECTION_THRESHOLD){
+		if(x_diff_compass > FROM_RIGHT_TO_LEFT_CORRECTION_THRESHOLD){
 		    printf("Rotate the floater to the left!\n");
 		    controlDCMotor(dc_motor,0);
 		    // controlServoOrientation(&servo,degree);
 		    
 		}
 		
-		else if(x_diff < FROM_LEFT_TO_RIGHT_CORRECTION_THRESHOLD){
+		else if(x_diff_compass < FROM_LEFT_TO_RIGHT_CORRECTION_THRESHOLD){
 		    printf("Rotate the floater to the right!\n");	
 		    controlDCMotor(dc_motor,1);
 		    // controlServoOrientation(&servo,degree);
 		    
 		}
 		
+		// Power management with acceleration data
+		printf("[MPU9250] Accel data [milli g] - Z: %hd\n", z_accel);
+		
+		float z_accell = ( (float) z_accel ) * GRAVITY_ACCELERATION / 1000;
+		
+		// Converting to string for printing purposes
+		char* z_accel_s = fromFloatToString(z_accell);
+		printf("[MPU9250] Accel data [m/s^2] - Z: %s\n", z_accel_s);
+		
+		powerManagement(z_accell);
+		
 		if(i == 0 || i == 1){
 		    i++;
 		}
 		
-		xtimer_usleep(DELAY_CONTROL);
+		xtimer_usleep(delay_control);
 	}
 	
 	return NULL;
@@ -274,7 +339,7 @@ static void* threadAlarm(void* arg)
 			controlBuzzer(buzzer,0);
 		
 		
-		xtimer_sleep(DELAY_ALARM);
+		xtimer_sleep(delay_alarm);
 	
 	}
 	
@@ -300,7 +365,7 @@ static void* threadInfo(void* arg)
 		I2CCommunication(INFO_CMD,buffer,INFO_BUFFER_SIZE,"INFO");
 		printf("[INFO]MEX: %s \n",buffer);
 	
-		xtimer_sleep(DELAY_INFO);
+		xtimer_sleep(delay_info);
 	
 	}
 	
