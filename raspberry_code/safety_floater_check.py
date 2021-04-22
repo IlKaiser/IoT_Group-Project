304edb328b124237ed3344c6d794e612bd1fa4e6
IlKaiser <samuele.rinaldi90@gmail.com>
2021-04-22 21:29:06 +0200

    Initial support for picamera and tensorflow

---
+++ raspberry_code/safety_floater_check.py
@@ -0,0 +1,36 @@
+import os
+from picamera import PiCamera
+import tensorflow as tf
+from time import sleep
+
+#Shutfu Keras
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+classes = ["No_Boat","Boat"]
+def take_pic_and_evaluate():
+    #Import model
+    model = tf.keras.models.load_model('model.h5')
+    print("Model correctly loaded...")
+
+    #Start camera
+    camera = PiCamera()
+    camera.resolution = (2592, 1944)
+    #Take pic
+    print("Taking pic...")
+    # Camera warm-up time
+    sleep(2)
+    camera.capture('pic.jpg')
+    print("Pic taken,start processing...")
+
+    #Tf analysis
+    img = tf.keras.preprocessing.image.load_img('pic.jpg',target_size=(128,128))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array,0)
+    predictions_single = model.predict(img_array)
+    print(predictions_single)
+    response = int(predictions_single[0][0])
+    print(classes[response])
+    print("Done!")
+    if response == 0:
+        return False
+    return True
