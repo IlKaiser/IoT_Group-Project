b4c4c83dc97ca0370c09ed4f95430f608875751e
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2021-05-06 12:52:28 +0200

    No deprecations

--- dashboard/routes/views.js
+++ dashboard/routes/views.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+ 
+
+const check_login = require("../helper/check_login");
 
 router.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views", "login.html"));
@@ -10,5 +13,14 @@ router.get("/violations", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views", "violations.html"));
 });
 
+router.post("/login-request", async (req,res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+ 
+  check_login.check(username,password,
+    /*success*/  function(){ res.redirect("/violations"); },
+    /*failure*/  function(){ res.redirect("/"); }
+  );
+});
 
 module.exports = router;
