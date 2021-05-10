995d05817d408cc4c13c39d2cddba30cf0266add
IlKaiser <samuele.rinaldi90@gmail.com>
2021-05-10 23:19:05 +0200

    Drive uploader support in main and misc fixes

--- raspberry_code/main.py
+++ raspberry_code/main.py
@@ -1,8 +1,13 @@
-import bluetooth_module
-import safety_floater_check
-import mqtt_module
+from modules.bluetooth_module      import bt_recieveLine
+from modules.drive_uploader_module import drive_upload
+from modules.mqtt_module           import mqtt_publish
+from modules.safety_floater_check  import sf_check
+
+
 
 import time
+
+photo_counter = 0
 
 print("##########################")
 print("# Safety Monitor Started #")
@@ -12,21 +17,35 @@ continue_counter = 0
 while(True):
     print("Waiting for message to come...")
     try:
-        lat,long,speed,id = bluetooth_module.recieveLine()
+        lat,long,speed,floater_id = bt_recieveLine()
         
         print("Checking if there is an actual violation...")
-        is_boat = safety_floater_check.check()
+        
+        ### Incremental Name
+        photo_counter += 1
+        photo_name     = "pic_{photo_counter}.jpg"
+         
+        is_boat = sf_check(photo_name=photo_name)
         
         if(is_boat):
-            print("It is a Boat! Notifying to DB...")
-            '''
-                TBD -> code for image uploading
-            '''
-            mqtt_module.mqtt_publish(lat,long,speed,id)
+            print("It is a Boat!")
+            
+            
+            ## Upload Pic
+            print("Uploading pic to Google Drive...")
+            file_id = drive_upload(photo_name=photo_name)
+            
+            ## Everything should be fine
+            assert(file_id != -1)
+            ## Publish to AWS mqtt
+            print("Notifying to DB...")
+            mqtt_publish(lat,long,speed,floater_id,file_id)
+            
         else:
             print("No actual Boat detected")
             
-    except:
+    except Exception as e:
+        
         continue_counter += 1
         if(continue_counter is 3):
             print("Too Many Errors...")
@@ -34,6 +53,8 @@ while(True):
             quit()
         else:
             print("Error Detected! Continue... "+ str(continue_counter))
-            print("Check if 'sudo rfcomm connect 0 <MAC of Bt>' is running")
+            print("\033[1;37;43m" +str(e)+"\033[0m")
+            if(e.args[0]is 2):
+                print("Check if 'sudo rfcomm connect 0 <MAC of Bt>' is running")
             time.sleep(5)
             continue
